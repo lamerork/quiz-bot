bf30702439a9c465342e8d36b16654e9b61580b6
Anton Ozerov <lamerork@gmail.com>
2024-01-17 20:30:01 +0300

    add telegram bot

--- telegram_bot.py
+++ telegram_bot.py
@@ -4,10 +4,9 @@ import logging
 import redis
 from telegram import Update, Bot, ReplyKeyboardRemove
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler
-from functools import partial
 
 from telegram_log import TelegramLogsHandler
-from utils import new_question, reply_markup
+from common import new_question, reply_markup
 
 
 CHOOSING, ANSWERING = range(2)
@@ -15,15 +14,16 @@ CHOOSING, ANSWERING = range(2)
 logger = logging.getLogger('Logger')
 
 
-def start(update, _):
+def start(update, context):
 
     update.message.reply_text('Здравствуйте! Я бот для викторин!')
     user = update.effective_user
 
     update.message.reply_text(
-        f'{user.name}\nДавай поиграем!\nДля вопроса нажми «Новый вопрос»',
+        f'{user.name}\nДля вопроса нажми «Новый вопрос»',
         reply_markup=reply_markup,
      )
+    context.user_data['score'] = 0
     return CHOOSING
 
 
@@ -51,6 +51,7 @@ def handle_solution_attempt(update, context):
     if user_answer.lower() == correct_answer.lower():
         text = 'Правильно! Поздравляю! Для следующего вопроса нажми «Новый вопрос»'
         update.message.reply_text(text, reply_markup=reply_markup)
+        context.user_data['score'] += 1
         return ANSWERING
     else:
         text = 'Неправильно… Попробуешь ещё раз?'
@@ -59,11 +60,19 @@ def handle_solution_attempt(update, context):
 
 def handle_score_request(update, context):
 
+    score = context.user_data['score']
+    text = f'Ваш счет: {score}'
+    update.message.reply_text(text, reply_markup=reply_markup)
     print('Счет')
 
 
 def handle_show_answer(update, context):
 
+    answer = context.user_data['answer']
+    text = f'Правильны ответ:\n{answer}'
+    update.message.reply_text(text, reply_markup=reply_markup)
+    update.message.reply_text(text, reply_markup=reply_markup)
+    handle_new_question_request(update, context)
     print('Сдаться')
 
 
@@ -82,7 +91,7 @@ def main():
 
     logger.info('Бот запущен')
 
-    redis_db = redis.Redis(host='localhost', port=6379, protocol=3, db=0)
+    #redis_db = redis.Redis(host='localhost', port=6379, protocol=3, db=0)
 
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler('start', start)],
